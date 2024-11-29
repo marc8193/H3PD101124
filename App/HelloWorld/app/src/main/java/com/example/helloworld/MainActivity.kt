01da413fb356894cf88ec723bc94829c2369231d
Marcus Haukelid Larsen <mhl@rtx.dk>
2024-11-29 12:31:46 +0100

    Update

---
+++ App/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.helloworld.ui.theme.HelloWorldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HelloWorldTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Counter(Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Counter(modifier: Modifier) {
+    var count by remember { mutableIntStateOf(0) }
+
+    Row(modifier = modifier) {
+        Text("$count", modifier = modifier)
+
+        Button(onClick = { count++ }, modifier) {
+            Text("+1")
+        }
+
+    }
+}
